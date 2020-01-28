f1a3bbc549fffb5f05708897b2b7addb7d8444a4
Artyom Danilin <artyom.danilin@mera.com>
2020-01-28 12:38:22 +0300

    Mini dashboard component was added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { MainBoardComponent } from "./components/main-board/main-board.component";
 import { BigDashboardComponent } from "./components/big-dashboard/big-dashboard.component";
+import { MiniDashboardComponent } from './components/mini-dashboard/mini-dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainBoardComponent,
-    BigDashboardComponent
+    BigDashboardComponent,
+    MiniDashboardComponent
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
---
+++ src/app/components/mini-dashboard/mini-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mini-dashboard',
+  templateUrl: './mini-dashboard.component.html',
+  styleUrls: ['./mini-dashboard.component.css']
+})
+export class MiniDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
