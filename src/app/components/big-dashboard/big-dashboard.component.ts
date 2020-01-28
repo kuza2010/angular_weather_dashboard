6214bbc0e932fc59168a668c114173fde2d17ac5
Artyom <danilinartem1997@gmail.com>
2020-01-28 22:16:20 +0300

    Components were updated
    update medium, big dashboards

--- src/app/components/big-dashboard/big-dashboard.component.ts
+++ src/app/components/big-dashboard/big-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherServiceService } from '../../services/weather-service.service'
 
 @Component({
   selector: 'app-big-dashboard',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BigDashboardComponent implements OnInit {
 
-  constructor() { }
+  temperature: number
+  description: string
+  humidity: number
+  windSpeed: number;
+
+
+  constructor(private weatherServise: WeatherServiceService) {
+  }
 
   ngOnInit() {
+    this.weatherServise.getWeatherForToday()
+      .then(weather => {
+        this.temperature = Math.floor(weather.main.temp)
+        this.description = weather.weather[0].description
+        this.windSpeed = weather.wind.speed
+        this.humidity = weather.main.humidity;
+      })
+      .catch(reason => {
+        console.log('Oops...')
+      });
   }
 
 }
