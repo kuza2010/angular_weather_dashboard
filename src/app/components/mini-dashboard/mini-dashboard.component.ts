16e5ffc2368b13243e8ce8d5e42c0b1898c76ea8
Artyom Danilin <artyom.danilin@mera.com>
2020-01-28 13:12:18 +0300

    Separete big and mini dashboards
    Separete those dashboards inside single div.

--- src/app/components/mini-dashboard/mini-dashboard.component.ts
+++ src/app/components/mini-dashboard/mini-dashboard.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-mini-dashboard',
-  templateUrl: './mini-dashboard.component.html',
-  styleUrls: ['./mini-dashboard.component.css']
+  selector: "app-mini-dashboard",
+  templateUrl: "./mini-dashboard.component.html",
+  styleUrls: ["./mini-dashboard.component.css"]
 })
-export class MiniDashboardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class MiniDashboardComponent {
+  public days: string[] = ["Wednesday", "Thursday", "Friday", "Saturday"];
 }
